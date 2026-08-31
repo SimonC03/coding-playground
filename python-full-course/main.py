fdc723afb8f4e9b0f93862ec941881ec95cb9dd5
Simon Carlén <simon.carlen1@gmail.com>
2026-08-31 21:35:08 +0200

    feat: Refactor main.py for improved typecasting and add madlibs-game.py placeholder

--- python-full-course/main.py
+++ python-full-course/main.py
@@ -1,13 +1,40 @@
-first_name = "Simon"
+# Typecasting = The process of converting a variable from one data type to another
+# str(), int(), float(), bool()
 
-favorite_food = "Pizza"
-
-print(f"Hello {first_name}, you like {favorite_food}")
-
-age = 25.251
-
-print(age)
-
+name = "Bro Code"
+age = 25
+gpa = 4.9
 is_student = True
 
-print(f"Are you a student?: {is_student}")
+gpa = int(gpa)
+age = float(age)
+
+name = bool(name)
+
+
+# input() = A function that prompts the user to enter data
+# Returns the entered data as a string
+
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+
+age = int(age)
+age = age + 1
+
+print(f"Hello {name}! You are {age} years old")
+
+# Exercise 1 - Ractangle Area Calc
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+
+area = length * width
+print(f"The area is: {area} cm2")
+
+# Exercise 2 - Shopping Cart Program
+
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like to buy?: "))
+total = price * quantity
+print(int(total))
