18af1da6e3a0c6bd3484470c537c24ce318964d5
Simon Carlén <simon.carlen1@gmail.com>
2026-09-01 21:58:20 +0200

    magic-methods

--- python-full-course/foundations/object-orientation/class-methods.py
+++ python-full-course/foundations/object-orientation/class-methods.py
@@ -37,3 +37,55 @@ student2 = Student("Max", 4.0)
 
 print(Student.get_count())
 print(Student.get_average_gpa())
+
+
+# Magic methods = Dunder methods (double underscore) __init__, __str__, __eq__
+#                   They are automatically called by many of python's built-in operations
+#                   They allow developers to define or custimize the behavior of objects
+
+class Book:
+
+    def __init__(self, title, author, num_pages):
+        self.title = title
+        self.author = author
+        self.num_pages = num_pages
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+
+    def __eq__(self, other):
+        return self.title == other.title and self.author == other.author
+
+    def __lt__(self, other): # lt = less then
+        return self.num_pages < other.num_pages
+
+    def __gt__(self, other): # gt = greater then
+        return self.num_pages > other.num_pages
+
+    def __add__(self, other):
+        return self.num_pages + other.num_pages
+
+    def __contains__(self, keyword):
+        return keyword in self.title or keyword in self.author
+
+    def __getitem__(self, key):
+        if key == "title":
+            return self.title
+        elif key == "author":
+            return self.author
+        elif key == "num_pages":
+            return self.num_pages
+        else:
+            return f"Key {key} was not found"
+        
+
+book1 = Book("The Hobbit", "J.R.R. Tolkien", 310)
+book2 = Book("Harry potter", "J.K Rowling", 223)
+book3 = Book("The Lion", "C.S Lewis", 152)
+
+print(book1)
+print(book1 == book2)
+print(book1 + book2)
+
+print("Lion" in book3)
+print(book1['title'])
