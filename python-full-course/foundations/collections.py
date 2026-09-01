afccf76b320d1a8411bbbdad6d494ea04de4ccb2
Simon Carlén <simon.carlen1@gmail.com>
2026-09-01 10:42:17 +0200

    feat: Add examples for tuples, 2D lists, and dictionaries in collections.py

--- python-full-course/foundations/collections.py
+++ python-full-course/foundations/collections.py
@@ -43,8 +43,63 @@ print(fruits)
 
 # Tuple
 
+"""
 fruits = ("apple", "orange", "banana", "coconut", "coconut")
 
 print(fruits.count("coconut"))
 # fruits.add("orange") fungerar ej då de inte kan modifieras
 print(fruits)
+"""
+
+# 2D list 
+
+"""
+    
+fruits = ["apple", "banaan", "coconut"]
+vetegables = ["celery", "carrots"]
+meats = ["chicken", "fish", "turkey"]
+
+groceries = [fruits, vetegables, meats]
+
+for collection in groceries:
+    for food in collection:
+        print(food, end=" ")
+    print()
+
+"""
+
+# dictionary = a collection of {key:value} pairs ordered and changeable. No duplicates
+
+"""
+capitals = {"USA": "Washington D.C",
+            "India": "New Delhi",
+            "China": "Beijing",
+            "Russia": "Moscow"}
+
+print(capitals.get("USA")) # Ger värdet kopplat till nyckeln USA
+
+if capitals.get("India"):
+    print("Capital exists")
+
+capitals.update({"Germany": "Berlin"})
+capitals.update({"USA": "Detroit"}) #Uppdaterar befintlig nyckel om de finns
+# capitals.popitem()
+# capitals.clear()
+
+keys = capitals.keys()
+
+for key in capitals.keys():
+    print(key)
+
+values = capitals.values()
+
+for value in capitals.values():
+    print(value)
+
+items = capitals.items()
+print(items)
+
+for key, value in capitals.items(): # Hämtar Key och Value från capitals.items() som ger en dict_items
+    print(f"{key}: {value}")
+
+"""
