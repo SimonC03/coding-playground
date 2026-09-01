e58975b08d0693eed04e01267bfc118169628907
Simon Carlén <simon.carlen1@gmail.com>
2026-09-01 09:58:25 +0200

    feat: Add credit number and card examples with indexing and loops

---
+++ python-full-course/foundations/indexing.py
@@ -0,0 +1,4 @@
+credit_number = "1234-1234-1234-1234"
+
+print(credit_number[0:4:2]) # [start, end, step]
+print(credit_number[-1]) # ger sista position
---
+++ python-full-course/foundations/loops.py
@@ -0,0 +1,12 @@
+credit_card = "1234-1234-1234-1234"
+
+""""
+for x in credit_card:
+    print(x)
+"""
+
+for x in range(1, 12):
+    if x == 2:
+        break # continue skippar bara
+    else:
+        print(x)
