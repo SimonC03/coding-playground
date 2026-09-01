240dca8eeb6297d508270375d502846b8809d7c9
Simon Carlén <simon.carlen1@gmail.com>
2026-09-01 14:13:17 +0200

    feat: Add functions module with add_one and square functions

---
+++ python-full-course/foundations/modules/functions.py
@@ -0,0 +1,7 @@
+pi = 3.141592653589793
+
+def add_one(x):
+    return x + 1
+
+def square(x):
+    return x ** 2
---
+++ python-full-course/foundations/modules/main.py
@@ -0,0 +1,13 @@
+# import math
+import math as m # use this
+from math import pi # skip if possible
+import functions
+
+result = functions.add_one(20)
+number = functions.square(2)
+
+print(result)
+print(number)
+
+print(m.pi)
+print(pi)
